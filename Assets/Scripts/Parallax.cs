257e30aa67644cf81899069a23daaa29be7b1a0f
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2023-05-13 16:44:21 +0200

    i fixed your mom

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class Parallax : MonoBehaviour
+{
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class Parallax : MonoBehaviour
+{
+    public PixelPerfectCamera cam;
+    public Transform player;
+    Vector2 startPosition;
+
+    float startZ;
+
+    Vector2 travel => (Vector2)cam.transform.position - startPosition;
+    Vector2 parallaxFactor;
+
+    public void Start()
+    {
+        startPosition = transform.position;
+        startZ = transform.position.z;
+    }
+
+    public void Update()
+    {
+        transform.position = startPosition + travel;
+    }
+}
