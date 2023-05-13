0dcfe8eb3be40a93e8cf7ac5fafdea7ae39ce2ef
Mikołaj Kiszka <mikolajkiszka123@gmail.com>
2023-05-13 05:18:37 +0200

    lol

--- Assets/Scripts/EnemyMomevent.cs
+++ Assets/Scripts/EnemyMomevent.cs
@@ -8,12 +8,15 @@ public class EnemyMomevent : MonoBehaviour
     public float speed = 1f; // Controls the speed of the saw's movement
     public float amplitude = 1f; // Controls the distance of the saw's movement
     public float offset = 0f; // Offsets the starting position of the saw's movement
+    public float disapearingTime = 1.5f;
 
     private Vector2 initialPosition; // Stores the initial position of the saw
     private float time = 0f; // Stores the elapsed time since the start of the game
+    private SpriteRenderer sprite;
     void Start()
     {
         initialPosition = transform.position;
+        sprite = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
@@ -25,5 +28,7 @@ public class EnemyMomevent : MonoBehaviour
 
         // Update the position of the saw
         transform.position = new Vector2(x, y);
+            // Change the 'color' property of the 'Sprite Renderer'
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1f - Mathf.Sin(time*disapearingTime));
     }
 }
