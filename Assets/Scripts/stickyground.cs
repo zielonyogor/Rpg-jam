f78129e083fea025836b9d1f8b8b80f5379e011d
Maja <majakom863@gmail.com>
2023-05-14 15:11:44 +0200

    na na na na na na na na na robi spam

--- Assets/Scripts/stickyground.cs
+++ Assets/Scripts/stickyground.cs
@@ -4,34 +4,36 @@ using UnityEngine;
 
 public class stickyground : MonoBehaviour
 {
-    public float slowDownFactor = 0.5f;
+
+    public float slowDownFactor = 0.1f;
     public PlayerMovement script;
-    private float startingSpeed = 5f;
+    private float startingSpeed;
     private float slowedSpeed;
     public bool isSlowed;
 
     private void Start()
     {
-        startingSpeed = script.moveSpeed;
-        slowedSpeed = slowDownFactor * startingSpeed;
+        slowedSpeed = slowDownFactor * script.moveSpeed;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        isSlowed = true;
+        if (collision.CompareTag("Player"))
         {
+            Debug.Log("aass");
             script.moveSpeed = slowedSpeed;
         }
     }
 
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        isSlowed = false;
+   // private void OnTriggerExit2D(Collider2D collision)
+    //{
+       // isSlowed = false;
         //if (script.isGrounded)
         //{
         //    script.moveSpeed = startingSpeed;
         //}
-    }
+    //}
 }
 
 
